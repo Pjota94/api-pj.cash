d241913ddc98089183bb18b4e37b72bde345cdaf
pjota <juniorgurinhem9@gmail.com>
2023-01-06 16:49:02 -0300

    fix: add cors

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,16 @@ import transferRoutes from "./routes/transfer.routes";
 
 const app = express();
 
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
+
 app.use(express.json());
 
 app.use("/users", userRoutes);
